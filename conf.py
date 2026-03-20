3f211656d28036c531905ab91efdb947c890e7a4
kevinphilip221 <kevinphilip221@protonmail.com>
2026-03-20 19:59:07 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Download and Reinstall Malwarebytes Already Purchased (Activate on Windows 10 or 11 Easily)"
+html_title = "pcmatic.com/account to Full Protection: How to Log Into My PC Matic Account and Install PC Matic Already Purchased"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
